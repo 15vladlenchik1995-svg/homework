41fec5f1402ab3f4cb41b31249d1844e9fb75be4
Den <15vladlenchik1995@gmail.com>
2026-05-21 21:56:41 +0300

    сделал 4 задания из 8

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         // Задача №1 объявление переменных
@@ -69,8 +71,54 @@ public class Main {
 
         System.out.println("Если в компании работает "+worker+" сотрудника,то всего :"
                 +TotalNumber+" часов может быть поделено между сотрудниками");
-        System.out.println();
-        System.out.println();
+
+           // Домашнее задание №3
+           // Задача 1
+          byte b = 127;
+          short s = 30000;
+          int i = 1000000;
+          long l = 1000000000L;
+          float f = 1.5F;
+          double d = 5.52654256;
+          System.out.println("Значение переменной - b с типом byte равно : " +b + "\n" + "Значение " +
+                "переменной - s с типом short равно : "+s +"\n"+"Значение переменной - " +
+                "i с типом int равно : "+i+"\n"+"Значение переменной - l с типом long " +
+                "равно : "+l+"\n"+"Значение переменной - f c типом float " +
+                "равно :  "+f+"\n"+"Значение переменной - d с типом double равно : "+
+                d);
+        // Задание №2
+           float f1 = 27.12F;
+           long l1 = 987678965549L;
+           float f2 = 2.786F;
+           short s1 = 569;
+           short s2 = -159;
+           short s3 = 27897;
+           byte b1 = 67;
+           //Задание №3
+           byte master1 = 23;
+           byte master2 = 27;
+           byte master3 = 30;
+           int totalPaper = 480;
+           int student = totalPaper / (master1+master2+master3);
+           System.out.println("На каждого ученика рассчитано "+student+" листов бумаги");
+           //Задание №4
+           short min20 = 20/2;
+           short day = (60*24)/2;
+           int day3 = day*3;
+           int month = day*30;
+        System.out.println("За 20 минут машина производит "+min20+"  штук бутылок"+"\n"+"За сутки машина" +
+                " производит "+ day+" штук бутылок"+"\n"+"За три дня машина производит "+day3+" штук бутылок" +
+                "\n"+"За " + "месяц машина производит "+month+" штук бутылок");
+
+
+
+
+
+
+
+
+
+
 
 
 
