baf8822a53a949f9c05a63c9e33181861712a641
Den <15vladlenchik1995@gmail.com>
2026-05-22 01:54:24 +0300

    HW3 completed

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.sql.SQLOutput;
+import java.io.PrintStream;
 
 public class Main {
     public static void main(String[] args) {
@@ -109,6 +109,55 @@ public class Main {
         System.out.println("За 20 минут машина производит "+min20+"  штук бутылок"+"\n"+"За сутки машина" +
                 " производит "+ day+" штук бутылок"+"\n"+"За три дня машина производит "+day3+" штук бутылок" +
                 "\n"+"За " + "месяц машина производит "+month+" штук бутылок");
+        //Задание №5
+        short class1 = 120/(2+4);
+        int white = class1*2;
+        int brown = class1*4;
+        System.out.println("В школе где "+class1+" классов,нужно "+white+" банок белой " +
+                "краски и " +brown+" банок коричневой краски");
+        //Задание №6
+        int gramm = (5*80)+(200/100*105)+(2*100)+(4*70);
+        float kg = gramm /1000f;
+        System.out.println("Завтрак спортсмена составляет "+gramm+" грамм "+"\n"+
+                "Завтрак спортсмена составляет "+kg+" киллограмм");
+        //Задача №7
+        float wt = 7*1000f;
+        float day1 = wt/250f;
+        float day2 = wt/500f;
+        float grammAvg = (250+500)/2;
+        float days = wt/grammAvg;
+        int roundedDays = Math.round(days);
+        System.out.println("При похудении спортсмена на 250 грамм" +
+                " в день необходиммо "+day1+" дней"+"\n"+"При похудении спортсмена на" +
+                " 500 грамм в день необходимо "+day2+" дней "+"\n"+"В среднем " +
+                "спортсмен может худеть в день на "+grammAvg+" грамм,и " +
+                "для этого ему необходимо "+roundedDays+" дней");
+        //Задание №8
+        double masha1 = 67760;
+        double denis2 = 83690;
+        double kris3 = 76230;
+        double manyYear1 = (masha1+masha1 * 0.1)*12;
+        double manyYear2 = (denis2+denis2 * 0.1)*12;
+        double manyYear3 = (kris3 + kris3 * 0.1)*12;
+        double odds1 = manyYear1-(masha1*12);
+        double odds2 = manyYear2-(denis2*12);
+        double odds3 = manyYear3-(kris3*12);
+        System.out.printf("Маша теперь получает %.0f рублей.Годовой доход вырос на " +
+                "%.0f рублей \n",manyYear1,odds1);
+        System.out.printf("Денис теперь получает %.0f рублей.Годовой доход вырос на " +
+                "%.0f рублей \n",manyYear2,odds2);
+        System.out.printf("Кристина теперь получает %.0f рублей.Годовой доход вырос на " +
+                "%.0f рублей \n",manyYear3,odds3);
+
+
+
+
+
+
+
+
+
+
 
 
 
